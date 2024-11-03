813572861d5e27db93465e68b669d0766ee4b15d
NickKvizhinadze <nickkvizhinadze@gmail.com>
2024-11-03 08:40:06 +0400

    Added exceptions logging in traces

--- SimpleMarket.Payments.Api/Services/PaymentService.cs
+++ SimpleMarket.Payments.Api/Services/PaymentService.cs
@@ -1,4 +1,6 @@
-﻿using DotNetHelpers.Models;
+﻿using System.Diagnostics;
+using DotNetHelpers.Models;
+using OpenTelemetry.Trace;
 using SimpleMarket.Payments.Api.Domain;
 using SimpleMarket.Payments.Api.Infrastructure.Data;
 using SimpleMarket.Payments.Api.Models;
@@ -16,16 +18,25 @@ public class PaymentService: IPaymentService
 
     public async Task<Result> CreatePayment(CreatePaymentDto model, CancellationToken cancellationToken)
     {
-        var payment = new Payment
+        try
         {
-            CustomerId = model.CustomerId,
-            CorrelationId = model.CorrelationId,
-            PaymentMethod = model.PaymentMethod,
-            Amount = model.Amount
-        };
-
-        await _dbContext.Payments.AddAsync(payment, cancellationToken);
-        await _dbContext.SaveChangesAsync(cancellationToken);
-        return Result.SuccessResult();
+            var payment = new Payment
+            {
+                CustomerId = model.CustomerId,
+                CorrelationId = model.CorrelationId,
+                PaymentMethod = model.PaymentMethod,
+                Amount = model.Amount
+            };
+            
+            await _dbContext.Payments.AddAsync(payment, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Result.SuccessResult();
+        }
+        catch (Exception ex)
+        {
+            Activity.Current?.SetStatus(Status.Error);
+            Activity.Current?.RecordException(ex);
+            return Result.InternalErrorResult();
+        }
     }
 }
